c58f11710babb796e41f85c6f0d06e30e3f5df88
rykaset <setianirika123@gmail.com>
2025-01-15 10:59:30 +0700

    Add files via upload

---
+++ tugas.js
@@ -0,0 +1,58 @@
+let namaDepan ="Rika";
+let namaBelakang ="Setiani";
+
+document.write("nama saya adalah "+ namaDepan + " " + namaBelakang);
+
+let alamat ="ngaluran tegalarum"; 
+
+document.write("  saya tinggal di desa tegalarum");
+
+let usia ="15";
+
+document.write("  pada tahun 2024 ini usia saya adalah 15 tahun.");
+
+let ttl =("demak 1 januari 2009");
+
+document.write("  Saya lahir di Demak 1 januari 2009");
+
+let gender =("perempuan");
+
+document.write("  jenis kelamin saya adalah perempuan");
+
+let agama =("Islam");
+
+document.write("  saya beragama islam");
+
+let email =("@setiakawan");
+
+document.write("  Alamat email saya adalah @setiakawan");
+
+let berat =("38 kg")
+
+document.write("  Saya memiliki berat badan 38 kg");
+
+let tinggi=("153 cm")
+
+document.write("  dengan tinggi 153 cm.");
+
+let hobi=("Membaca");
+
+document.write("  saya sangat senang Membaca");
+
+let cita=("software engineering")
+
+document.write("  karena saya memiliki cita-cita menjadi software engenineer");
+
+let pekerjaan=("Pelajar");
+
+document.write("  saat ini saya adalah seorang pelajar");
+
+let sekolah=("Smkn 1 Sayung");
+
+document.write("  yang bersekolah di Smkn 1 Sayung");
+
+let jurusan=("PPLG")
+
+document.write("  pada jurusan PPLG")
+
+           
